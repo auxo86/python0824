1c1b4e1a04ea600bba87cee0f6d38ef902b9e7a2
auxo <auxo@a.b.com>
2017-08-24 20:46:50 +0800

    各種字串填充

---
+++ 底限做填充.py
@@ -0,0 +1,7 @@
+# encoding=utf-8
+print '{:_<10}'.format('測試')
+print '{:&<10}'.format('測試')
+print '{:\'<10}'.format('測試')
+print '{:*<10}'.format('測試')
+print '{:*<10}'.format('測試')
+print '{:>10}'.format('測試')
