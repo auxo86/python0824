85ef2fd6f669dd32447317705939e9bb369af3b3
auxo <auxo@a.b.com>
2017-08-24 19:39:01 +0800

    去UCI的網站上就可以抓到訓練的數據 zipline時序分析 scikit-learn

---
+++ demo7.py
@@ -0,0 +1,11 @@
+# encoding=utf-8
+
+string1 = 'Machine Learning in Python'
+list1 = list(string1)
+print list1
+list1.reverse()
+print list1
+list1.sort()
+print list1
+list1.sort(reverse=True)
+print list1
