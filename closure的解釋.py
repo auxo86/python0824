34fb1efb3b407e4194139ca4b6438e64842fb109
auxo <auxo@a.b.com>
2017-08-24 21:29:14 +0800

    lambda python用法

--- closure的解釋.py
+++ closure的解釋.py
@@ -9,3 +9,7 @@ print lengthArray
 
 # 上面等同於下面這行
 print [len(day) for day in day_of_week]
+
+day_of_week2 = ('Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturdat')
+print type(day_of_week), type(day_of_week2)
+print [len(day) for day in day_of_week2]
