daa38a8d8d79bca73f4fee8fe4dcf4c1ba9e9531
auxo <auxo@a.b.com>
2017-08-24 19:18:10 +0800

    numpy的矩陣練習

---
+++ demo6.5numpy練習.py
@@ -0,0 +1,9 @@
+import numpy as np
+x = [1, 2, 3]
+y = [4, 5, 6]
+print x, y, x + y
+
+arrayX = np.array(x)
+arrayY = np.array(y)
+print arrayX, arrayY, arrayX + arrayY
+print arrayX - arrayY, arrayX * arrayY
